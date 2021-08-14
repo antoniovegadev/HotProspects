e96bbbd7052d5c856a088613fc6ccd9ffbb0f645
antonivegaochoa <tonivegaochoa@gmail.com>
2021-08-14 11:42:19 -0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Antonio Vega on 8/14/21.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
