046dcf19a7efa143a2fc93926eb7a40fd2c965a0
antonivegaochoa <tonivegaochoa@gmail.com>
2021-08-15 14:47:26 -0500

    Building out tab bar

---
+++ HotProspects/Views/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Antonio Vega on 8/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Image(systemName: "person.3")
+                    Text("Everyone")
+                }
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Image(systemName: "checkmark.circle")
+                    Text("Contacted")
+                }
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Image(systemName: "questionmark.diamond")
+                    Text("Uncontacted")
+                }
+            MeView()
+                .tabItem {
+                    Image(systemName: "person.crop.square")
+                    Text("Me")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotProspects/Views/MeView.swift
@@ -0,0 +1,20 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by Antonio Vega on 8/15/21.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeView()
+    }
+}
